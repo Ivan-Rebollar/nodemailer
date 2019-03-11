a48a82937c29b1901c1f065150d7da1474d67c23
Ivan Rebollar <ivan@insaite.io>
2019-03-11 10:36:47 -0600

    OAuth con token y template

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+var fs = require('fs');
+//const xoauth2 = require('xoauth2');
 
+
+//usando configuraciones de SMTP y OAuth2
 let transporter = nodemailer.createTransport({
   service: 'gmail',
-  auth: {
-    user: '',
-    pass: ''
+  host:'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth:{
+    type:'OAuth2',
+    user: 'joivrh18@gmail.com',
+    clientId: '22305086248-qq12ce9040m4ts3hk1aacofbttiuo5h3.apps.googleusercontent.com',
+    clientSecret: 'X7SzIo5clwJ-RbA645TOF8ji',
+    refreshToken: '1/BewLvdqSrR-dnrgZ8HEMJgayF3GetoGlGzEGblIE6cA',
   }
 });
 
+//objeto email a enviar
 let mailOptions = {
-  from: '@gmail.com',
-  to: '@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!',
-  html:'<h1>Welcome</h1><p></p>'
-};
+  from: 'joivrh18@gmail.com',
+  to: 'macunamjirhpk@gmail.com',
+  subject: 'Prueba de correo',
+  //html:'<h1>Prueba HTML</h1><h3>código html con imagen attachada</h3>',
+  attachments:[{
+    path:'/home/ivan/Imágenes/Captura de pantalla de 2019-01-22 16-04-33.png'
+  }]
+}
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
+var filePath = './template/main.ejs';
+var compiled = ejs.compile(fs.readFileSync(filePath,'utf8'));
+mailOptions.html = compiled(mailOptions);
+
+//verificando conexión SMTP
+transporter.verify((error,success) => {
+  if(error){
     console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
+  }else{
+    console.log('Servidor listo para tomar el mensaje');
+    //enviando correo
+    transporter.sendMail(mailOptions,(err,info) => {
+      if(err){
+        console.log(err);
+      }else{
+        console.log(`Email enviado ${info.response}`);
+      }
+    });
   }
 });
